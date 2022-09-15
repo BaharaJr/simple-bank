f4aaec1e39a99f8a4f6db3b135e3baf9aa10cf13
Bennett Benedict <bennybenester@gmail.com>
2022-09-15 20:19:57 +0300

    add entries sqlc go models

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -10,12 +10,17 @@ import (
 
 type Querier interface {
 	CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error)
+	CreateEntry(ctx context.Context, arg CreateEntryParams) (Entry, error)
 	DeleteAccount(ctx context.Context, id int64) error
+	DeleteEntry(ctx context.Context, id int64) error
 	GenericSearch(ctx context.Context, arg GenericSearchParams) ([]Account, error)
 	GetAccountById(ctx context.Context, id int64) (Account, error)
 	GetAccountByOwner(ctx context.Context, owner string) (Account, error)
 	GetAccounts(ctx context.Context, arg GetAccountsParams) ([]Account, error)
+	GetEntries(ctx context.Context, arg GetEntriesParams) ([]Entry, error)
+	GetEntryId(ctx context.Context, id int64) (Entry, error)
 	UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error)
+	UpdateEntry(ctx context.Context, arg UpdateEntryParams) (Entry, error)
 }
 
 var _ Querier = (*Queries)(nil)
